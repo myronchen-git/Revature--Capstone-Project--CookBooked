f54feba8b5b632623bc37ead470214a864be3427
Jacob Ota <otajacob@yahoo.com>
2024-03-04 15:25:57 -0800

    Structured files and installed react and server side dependencies

---
+++ server/app.js
@@ -0,0 +1,26 @@
+//imports
+const express = require('express');
+const { logger } = require('./src/util/logger');
+
+//Routers
+//TODO
+
+//create the server on PORT 3000
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    logger.info(`Started the server on Port ${PORT}`);
+});
+
+//middleware to change body requests to json
+app.use(express.json());
+
+//middleware to log incoming requests
+app.use((req, res, next) => {
+    logger.info(`Incoming ${req.method}: ${req.url}`);
+    next();
+})
+
+//HTTP Routes: 
+//TODO
---
+++ server/src/util/logger.js
@@ -0,0 +1,20 @@
+const { createLogger, transports, format } = require("winston");
+
+//Winston
+const logger = createLogger({
+  level: "info", //means to log only messages with level 'info' and above
+  format: format.combine(
+    format.timestamp(),
+    format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level}]: ${message}`;
+    }),
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "app.log" }),
+  ],
+});
+
+module.exports = {
+    logger: logger
+}
