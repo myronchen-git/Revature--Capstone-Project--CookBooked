57c31d853ef9b306357d8ba41b895c14bcaab6b8
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-03-06 17:36:20 -0800

    feat: implement Service for getting reviews by recipe
    * Added getReviews function in ReviewService.js, which will handle any
    given valid parameters for retrieving reviews, and then call the
    appropriate DAO function.
    
    * In ReviewService.js, Prettier made stylistic changes to the top half
    of the file .

---
+++ server/__test__/service/ReviewsService.test.js
@@ -0,0 +1,121 @@
+const { getReviews } = require("../../src/service/ReviewsService");
+const ReviewsDAO = require("../../src/repository/ReviewsDAO");
+
+// --------------------------------------------------
+
+const RECIPE_ID1 = "1234";
+const REVIEW_ID1 = "987";
+const REVIEW_ID2 = "654";
+const AUTHOR1 = "author1";
+const AUTHOR2 = "author2";
+const MAX_LIMIT = 100;
+
+// ==================================================
+
+describe("getReviews", () => {
+  test(
+    "When given a recipe ID property, this should call the " +
+      "DAO's getReviewsByRecipeId and return a list of reviews.",
+    async () => {
+      // Arrange
+      const REQUEST_QUERY_PARAMS = { recipeId: RECIPE_ID1 };
+      const EXPECTED_PASSED_PROPS = { ...REQUEST_QUERY_PARAMS, Limit: MAX_LIMIT };
+      const EXPECTED_RESULT = {
+        items: [
+          { recipeId: RECIPE_ID1, reviewId: REVIEW_ID1, author: AUTHOR1 },
+          { recipeId: RECIPE_ID1, reviewId: REVIEW_ID2, author: AUTHOR2 },
+        ],
+        LastEvaluatedKey: {},
+      };
+
+      const getReviewsByRecipeIdSpy = jest
+        .spyOn(ReviewsDAO, "getReviewsByRecipeId")
+        .mockReturnValue(structuredClone(EXPECTED_RESULT));
+
+      // Act
+      const RESULT = await getReviews(REQUEST_QUERY_PARAMS);
+
+      // Assert
+      expect(RESULT).toEqual(EXPECTED_RESULT);
+      expect(getReviewsByRecipeIdSpy).toHaveBeenCalledTimes(1);
+      expect(getReviewsByRecipeIdSpy).toHaveBeenCalledWith(EXPECTED_PASSED_PROPS);
+    }
+  );
+
+  test(
+    "When given recipe ID, ExclusiveStartKey, and Limit, this should call the " +
+      "DAO's getReviewsByRecipeId with the correct properties and return a list of reviews.",
+    async () => {
+      // Arrange
+      const REQUEST_QUERY_PARAMS = {
+        recipeId: RECIPE_ID1,
+        ExclusiveStartKey: { recipeId: RECIPE_ID1, reviewId: REVIEW_ID1 },
+        Limit: 20,
+      };
+      const EXPECTED_PASSED_PROPS = structuredClone(REQUEST_QUERY_PARAMS);
+      const EXPECTED_RESULT = {
+        items: [{ recipeId: RECIPE_ID1, reviewId: REVIEW_ID2, author: AUTHOR2 }],
+        LastEvaluatedKey: {},
+      };
+
+      const getReviewsByRecipeIdSpy = jest
+        .spyOn(ReviewsDAO, "getReviewsByRecipeId")
+        .mockReturnValue(structuredClone(EXPECTED_RESULT));
+
+      // Act
+      const RESULT = await getReviews(REQUEST_QUERY_PARAMS);
+
+      // Assert
+      expect(RESULT).toEqual(EXPECTED_RESULT);
+      expect(getReviewsByRecipeIdSpy).toHaveBeenCalledTimes(1);
+      expect(getReviewsByRecipeIdSpy).toHaveBeenCalledWith(EXPECTED_PASSED_PROPS);
+    }
+  );
+
+  test("Giving a Limit of less than 1 should throw an error.", () => {
+    // Arrange
+    const REQUEST_QUERY_PARAMS = { recipeId: RECIPE_ID1, Limit: 0 };
+    const getReviewsByRecipeIdSpy = jest.spyOn(ReviewsDAO, "getReviewsByRecipeId");
+
+    // Act
+    async function runFunc() {
+      await getReviews(REQUEST_QUERY_PARAMS);
+    }
+
+    // Assert
+    expect(runFunc).rejects.toThrow();
+    expect(getReviewsByRecipeIdSpy).not.toHaveBeenCalled();
+  });
+
+  test("Giving a Limit of more than 100 should throw an error.", () => {
+    // Arrange
+    const REQUEST_QUERY_PARAMS = { recipeId: RECIPE_ID1, Limit: 101 };
+    const getReviewsByRecipeIdSpy = jest.spyOn(ReviewsDAO, "getReviewsByRecipeId");
+
+    // Act
+    async function runFunc() {
+      await getReviews(REQUEST_QUERY_PARAMS);
+    }
+
+    // Assert
+    expect(runFunc).rejects.toThrow();
+    expect(getReviewsByRecipeIdSpy).not.toHaveBeenCalled();
+  });
+
+  // This test is temporary, until there are GET requests for reviews using the author or createdAt attributes.
+  test("When not given a recipe ID property, this should throw an error.", async () => {
+    // Arrange
+    const REQUEST_QUERY_PARAMS = {};
+
+    const getReviewsByRecipeIdSpy = jest.spyOn(ReviewsDAO, "getReviewsByRecipeId");
+
+    // Act
+    async function runFunc() {
+      await getReviews(REQUEST_QUERY_PARAMS);
+    }
+
+    // Assert
+    expect(runFunc).rejects.toThrow();
+    expect(getReviewsByRecipeIdSpy).not.toHaveBeenCalled();
+  });
+});
--- server/src/service/ReviewsService.js
+++ server/src/service/ReviewsService.js
@@ -1,32 +1,83 @@
-const reviewsDao = require('../repository/ReviewsDAO');
-const uuid = require('uuid');
-const { logger } = require('../util/logger');
+const reviewsDao = require("../repository/ReviewsDAO");
+const uuid = require("uuid");
+const { logger } = require("../util/logger");
+const ArgumentError = require("../errors/ArgumentError");
+
+// ==================================================
 
 async function createNewReview(receivedData) {
-    logger.info('CreateNewReview method called');
-    //validate data before posting a new review
-    try {
-        //create new review
-        const review = {
-            recipeId: receivedData.recipeId, //recipeId passed from MealDB API
-            reviewId: uuid.v4(),
-            author: receivedData.username, //username acquired from JWT
-            imageUrl: receivedData.imageUrl, //passed in by user
-            rating: receivedData.rating, //passed in by user
-            content: receivedData.content, //passed in by user
-            createdAt: Date.now()
-        }
-        //create the new post
-        const data = await reviewsDao.postReview(review);
-        //return the review object to show back
-        return data;
-    } catch(err) {
-        logger.error(err);
-        throw Error(err.message);
-    }
+  logger.info("CreateNewReview method called");
+  //validate data before posting a new review
+  try {
+    //create new review
+    const review = {
+      recipeId: receivedData.recipeId, //recipeId passed from MealDB API
+      reviewId: uuid.v4(),
+      author: receivedData.username, //username acquired from JWT
+      imageUrl: receivedData.imageUrl, //passed in by user
+      rating: receivedData.rating, //passed in by user
+      content: receivedData.content, //passed in by user
+      createdAt: Date.now(),
+    };
+    //create the new post
+    const data = await reviewsDao.postReview(review);
+    //return the review object to show back
+    return data;
+  } catch (err) {
+    logger.error(err);
+    throw Error(err.message);
+  }
 }
 
+/**
+ * Gets reviews belonging to a recipe or author.  Also gets reviews that are ordered by creation time.
+ *
+ * @param {Object} requestQueryParams Object that may contain String recipeId or String author.
+ * May contain Object ExclusiveStartKey and Number Limit.
+ * @returns {{items, LastEvaluatedKey}} An Object containing an Array of Review Objects and LastEvaluatedKey.
+ * LastEvaluatedKey may be empty.
+ * @throws Currently, an Error if props argument does not contain String recipeId.
+ */
+async function getReviews(requestQueryParams) {
+  logger.info(`ReviewsService.getReviews(${JSON.stringify(requestQueryParams)})`);
+
+  const MAX_LIMIT = 100;
+
+  let reviewsData;
+  if (requestQueryParams.recipeId) {
+    const { Limit } = requestQueryParams;
+
+    // max limit of 100 is arbitrary
+    if (Limit !== undefined && (Limit <= 0 || Limit > MAX_LIMIT)) {
+      throw new ArgumentError("Argument 'Limit' is outside of allowed range.  Range is 1 to 100.");
+    }
+
+    // constructing PROPS
+    const PROPS = (({ recipeId, ExclusiveStartKey, Limit }) => ({
+      recipeId,
+      ExclusiveStartKey,
+      Limit,
+    }))(requestQueryParams);
+
+    // checking for a Limit
+    PROPS.Limit = PROPS.Limit || MAX_LIMIT;
+
+    reviewsData = await reviewsDao.getReviewsByRecipeId(PROPS);
+  } else {
+    // TODO
+    // query author-createdAt-index
+    // scan createdAt-index
+    logger.error("Getting reviews by other query parameters are not yet supported.");
+    throw new Error("Getting reviews by other query parameters are not yet supported.");
+  }
+
+  logger.info(`ReviewsService.getReviews: Returning ${JSON.stringify(reviewsData)}`);
+  return reviewsData;
+}
+
+// ==================================================
 
 module.exports = {
-    createNewReview
-}
+  createNewReview,
+  getReviews,
+};
