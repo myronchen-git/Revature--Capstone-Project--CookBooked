c4f038335d10f0f540eb6207de064338fe6f9cc8
Jacob Ota <otajacob@yahoo.com>
2024-03-15 00:04:45 -0700

    Linked all cards to respective pages, created header for the recipe page with picture, instructions, ingredients, sources

--- client/src/Components/BrowseRecipes/BrowserRecipeView.tsx
+++ client/src/Components/BrowseRecipes/BrowserRecipeView.tsx
@@ -1,30 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import './BrowserRecipeView.css'
+import './BrowserRecipeView.css';
+import { Link } from 'react-router-dom';
 
 function BrowserRecipeView(props: any) {
     //state to hold all the recipes from a category
    const [recipes, setRecipes] = useState<any[]>([]);
    //check if loaded
    const [loaded, setLoaded] = useState(false);
-  
-    useEffect(() => {
-      //allow the categories to load before the effect
-      setLoaded(false)
-      //on render and after the categories are populated run the getAllRecipes
-      getAllRecipes();
-      setLoaded(true);
-    }, [props.categories])
+   
+  useEffect(() => {
+    //allow the categories to load before the effect
+    setLoaded(false)
+    //on render and after the categories are populated run the getAllRecipes
+    getAllRecipes();
+    setLoaded(true);
+  }, [props.categories])
     
-    //this useEffect will grab the items from TheMealDB and place them in the recipes state
-    useEffect(() => {
-      setLoaded(false)
-      //on render and after the categories are populated run the getAllRecipes
-      getAllRecipes();
-      setLoaded(true);
-    }, [props.selectedItem])
+  //this useEffect will grab the items from TheMealDB and place them in the recipes state
+  useEffect(() => {
+    setLoaded(false)
+    //on render and after the categories are populated run the getAllRecipes
+    getAllRecipes();
+    setLoaded(true);
+  }, [props.selectedItem])
 
-    /**
+  /**
    * Function calls the MealDB to grab All of the category recipes
    */
   function getAllRecipes() {
@@ -59,6 +60,10 @@ function BrowserRecipeView(props: any) {
     function altText(recipe: string) {
       return `Image of ${recipe}`;
     }
+
+    function recipeRoute(id: string) {
+      return `/recipe/${id}`;
+    }
       
     return (
     <>
@@ -68,19 +73,21 @@ function BrowserRecipeView(props: any) {
                 recipes.map((recipe, idx) => {
                     return(
                         <div className='col-sm-8 col-md-3 mx-5 gy-5 m-5'>
-                          <div className='row background-card-color rounded p-4'>
-                            <div className='col-12 d-flex justify-content-center'>
-                              <div className='img-container'>
-                                <img src={recipe.strMealThumb} alt={altText(recipe.strMeal)} className='img-fluid'></img>
+                          <Link to={recipeRoute(recipe.idMeal)}>
+                            <div className='row bg-light rounded p-4'>
+                              <div className='col-12 d-flex justify-content-center'>
+                                <div className='img-container'>
+                                  <img src={recipe.strMealThumb} alt={altText(recipe.strMeal)} className='img-fluid'></img>
+                                </div>
+                              </div>
+                              <div className='col-10 text-danger text-decoration-underline text-sm text-md text-lg d-flex justify-content-center m-4 text-wrap'>
+                                <h3>{recipe.strMeal}</h3>
+                              </div>
+                              <div className='col-12 text-danger text-sm text-md text-lg d-flex justify-content-center'>
+                                <h5>Click Card For More</h5>
                               </div>
                             </div>
-                            <div className='col-10 text-danger text-decoration-underline text-sm text-md text-lg d-flex justify-content-center m-4 text-wrap'>
-                              <h3>{recipe.strMeal}</h3>
-                            </div>
-                            <div className='col-12 text-danger text-sm text-md text-lg d-flex justify-content-center'>
-                              <h5>Click Card For More</h5>
-                            </div>
-                          </div>
+                          </Link>
                         </div>
                     )
                 })
