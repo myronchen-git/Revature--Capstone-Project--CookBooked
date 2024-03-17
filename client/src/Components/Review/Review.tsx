a6bb762c18cbd808c98fa5b56249bf9c5931a65d
Jacob Ota <otajacob@yahoo.com>
2024-03-16 21:21:31 -0700

    My mistake, accidentally thought recipe was the review page

--- client/src/Components/Review/Review.tsx
+++ client/src/Components/Review/Review.tsx
@@ -1,33 +1,9 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import RecipeView from './RecipeView';
+import React from 'react'
 
-function Recipe() {
-  //grab the recipesId
-  const { recipeId } = useParams();
-
-  const [recipeInfo, setRecipeInfo] = useState(null);
-    
-  useEffect(() => {
-    axios.get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + recipeId)
-      .then((response: any) => {
-        setRecipeInfo(response.data.meals)
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }, [])
-
-  if(recipeInfo === null) {
-    return <div></div>
-  }
-
+function Review() {
   return (
-    <>
-      <RecipeView recipeInfo={recipeInfo} />
-    </>
+    <div>Review</div>
   )
 }
 
-export default Recipe
+export default Review
