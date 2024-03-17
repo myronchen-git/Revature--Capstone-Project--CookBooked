5796f24f227544f3ae12455c1e4af15ff5da85eb
bradleyc95 <bradleycox1995@gmail.com>
2024-03-17 15:12:50 -0400

    added error display when login or registration fails

--- client/src/Components/Login-Register/Register/RegisterContainer.tsx
+++ client/src/Components/Login-Register/Register/RegisterContainer.tsx
@@ -9,6 +9,7 @@ const URL = `http://localhost:4000/accounts`;
 function RegisterContainer() {
     let dispatch = useDispatch();
     const [success, setSuccess] = useState(false);
+    const [isError, setIsError] = useState(false);
 
     async function createUser(user: any) {
         try {
@@ -18,10 +19,12 @@ function RegisterContainer() {
             });
             if (response.status == 201) {
                 setSuccess(true);
+                setIsError(false);
             }
             console.log(response);
             return response;
         } catch (err) {
+            setIsError(true);
             console.log(err);
         }
     }   
@@ -30,6 +33,7 @@ function RegisterContainer() {
     <>
         {!success && <RegisterInput createUser={createUser} />}
         {success && <p>Account creation successful, you may now login with your credentials</p>}
+        {isError && <ErrorView message={'Account creation failed, please try again with different credentials'} isError={true} />}
     </>
   )
 }
