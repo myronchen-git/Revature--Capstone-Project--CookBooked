d5abd578c674f75088b5ae03fa3fd5f6124833aa
Jacob Ota <otajacob@yahoo.com>
2024-03-16 21:16:30 -0700

    Git pull, removed my recipe files, but still present on Github, readded and pushed

--- client/src/Components/Review/Review.tsx
+++ client/src/Components/Review/Review.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import RecipeView from './RecipeView';
 
-function Review() {
+function Recipe() {
+  //grab the recipesId
+  const { recipeId } = useParams();
+
+  const [recipeInfo, setRecipeInfo] = useState(null);
+    
+  useEffect(() => {
+    axios.get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + recipeId)
+      .then((response: any) => {
+        setRecipeInfo(response.data.meals)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, [])
+
+  if(recipeInfo === null) {
+    return <div></div>
+  }
+
   return (
-    <div>Review</div>
+    <>
+      <RecipeView recipeInfo={recipeInfo} />
+    </>
   )
 }
 
-export default Review
+export default Recipe
