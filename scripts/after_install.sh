23cf6ccc6184cfff0e87f084b1c62c9ec000f720
Jacob Ota <otajacob@yahoo.com>
2024-03-17 20:05:38 -0700

    Redid scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo "Running the after_install.sh script..."
 cd /home/ubuntu/CookBooked/server
 npm install
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo "Running the app_start.sh script..."
 cd /home/ubuntu/CookBooked/server
 node app.js > /dev/null 2>&1 &
