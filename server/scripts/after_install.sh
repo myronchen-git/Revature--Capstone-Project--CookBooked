ff042ccb84879f084fd20380611a715d7ed7ad6c
Jacob Ota <otajacob@yahoo.com>
2024-03-17 19:34:18 -0700

    Added echos to see if it reaches scripts

--- server/scripts/after_install.sh
+++ server/scripts/after_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo "Running the after_install.sh script..."
 cd /home/ubuntu/CookBooked/server
 npm install
--- server/scripts/application_start.sh
+++ server/scripts/application_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo "Running the app_start.sh script..."
 cd /home/ubuntu/CookBooked/server
 node app.js > /dev/null 2>&1 &
